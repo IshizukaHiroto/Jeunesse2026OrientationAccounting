bf45ebc4adcc6b70c25826fa2157e97909f5d8a1
Hiroto Ishizuka <2455111t@stu.kobe-u.ac.jp>
2026-03-05 02:18:36 +0900

    URLを張り替えた

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyjnRUYvYMveuf2WjjSenJnlF_L6t11jpmuSFWRo2N6taDHzc5fEN_sRbFw3a2k50YAyA/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbzQTziX4JVPvPL-vM_MxTNBr1enpDylXSZng9jQlJRjZeZqw9XuV30Zl0ac_tRZMNYqpg/exec",
   POLLING_MS: 60000,
   REQUEST_TIMEOUT_MS: 12000
 };
