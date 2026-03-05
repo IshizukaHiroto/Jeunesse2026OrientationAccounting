670e61092a1a04311ea3cacc317de435a26822ce
Hiroto Ishizuka <2455111t@stu.kobe-u.ac.jp>
2026-03-05 15:17:33 +0900

    Add balance breakdown pie

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbzQTziX4JVPvPL-vM_MxTNBr1enpDylXSZng9jQlJRjZeZqw9XuV30Zl0ac_tRZMNYqpg/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbx92cycdy4pdwgUyZu8odFp0jQIbJa_ypewMWfKDvuQUwClhjWXPZgJrMV0WABUIydLfA/exec",
   POLLING_MS: 60000,
   REQUEST_TIMEOUT_MS: 12000
 };
